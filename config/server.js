030470653e1033a839338482bce145cd59efc474
arman <MY_NAME@example.com>
2021-05-27 11:14:38 +0430

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 5001),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b03cee5ab691b9e9b43bbc8ea557dae4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9fad2b3e-e0ae-4daa-b367-54022844f2b5'
+};
