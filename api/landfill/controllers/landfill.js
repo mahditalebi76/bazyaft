748b34efbe415baa121c8535dcd332365d7f4afa
arman <nemati.arman@yahoo.com>
2021-06-11 13:27:01 +0430

    findnameandid

--- api/landfill/controllers/landfill.js
+++ api/landfill/controllers/landfill.js
@@ -4,5 +4,24 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+dule.exports = {
 
-module.exports = {};
+
+  findNameAndId:
+    async (ctx) => {
+      const result = await strapi.query("landfills").model.fetchAll({
+        columns: ["id", "Name"],
+        // withRelated: [
+        //   {
+        //     'basecategory': qb => {qb.columns("id","name")},
+        //     'image': qb => {qb.columns('id','url')},
+        //   },
+        // ],
+      });
+      ctx.send(result)
+    },
+
+
+
+};
+
